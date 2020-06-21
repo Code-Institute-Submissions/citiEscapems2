85766ea34eaea31663c24e95709fd97278320fb5
TheBigBushman <alanbushell91@gmail.com>
2020-06-21 18:10:20 +0000

    updated structure of page. fixed nav bar on small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 
 	var myCity = [
-	    {'name':'Dublin', 'age':'assets/img/dublin.jpg', 'birthdate':'11/10/1989'},
-	    {'name':'Milan', 'age':'32', 'birthdate':'10/1/1989'},
-	    {'name':'Paul', 'age':'29', 'birthdate':'10/14/1990'},
-	    {'name':'Dennis', 'age':'25', 'birthdate':'11/29/1993'},
-	    {'name':'Tim', 'age':'27', 'birthdate':'3/12/1991'},
-	    {'name':'Erik', 'age':'24', 'birthdate':'10/31/1995'},
+	    {'name':'Dublin', 'age':'assets/img/dublin.jpg', 'country':'Ireland'},
+	    {'name':'Milan', 'age':'32', 'country':'Italy'},
+	    {'name':'Berlin', 'age':'29', 'country':'Germany'},
+	    {'name':'Paris', 'age':'25', 'country':'France'},
+	    {'name':'London', 'age':'27', 'country':'United Kingdom'},
+	    {'name':'Vienna', 'age':'24', 'country':'Austria'},
 	]
 	
 	cityTile(myCity)
@@ -16,12 +16,14 @@
 		var table = document.getElementById('myTile')
 
 		for (var i = 0; i < data.length; i++){
-			var tile = `<div class="tile">
-                            <li>
+			var tile = `<div class="row">
+            <div class="col col-sm-12 col-md-6 col-lg-3>
+                            <li class="tile">
                             <img src="${data[i].age}" alt="city name" />
 							<h3>${data[i].name}</h3>
-							<div>${data[i].birthdate}</div>
+							<div>${data[i].country}</div>
 					  </li>
+                      </div>
                       </div>`
 			table.innerHTML += tile
 		}
