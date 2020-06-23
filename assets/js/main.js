a432a8aa25866e61c8459285ef645b0437ba88f6
TheBigBushman <alanbushell91@gmail.com>
2020-06-23 13:23:05 +0000

    updated map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 	var myCity = [
-	    {'name':'Dublin', 'age':'assets/img/dublin.jpg', 'country':'Ireland'},
+	    {'name':'Dublin', 'img':'assets/img/dublin.jpg', 'country':'Ireland'},
 	    {'name':'Milan', 'age':'32', 'country':'Italy'},
 	    {'name':'Berlin', 'age':'29', 'country':'Germany'},
 	    {'name':'Paris', 'age':'25', 'country':'France'},
@@ -8,9 +8,9 @@
 	    {'name':'Vienna', 'age':'24', 'country':'Austria'},
 	]
 	
-	cityTile(myCity)
+	
 
-
+cityTile(myCity)
 
 	function cityTile(data){
 		var table = document.getElementById('myTile')
@@ -19,7 +19,7 @@
 			var tile = `<div class="row">
             <div class="col col-sm-12 col-md-6 col-lg-3>
                             <li class="tile">
-                            <img src="${data[i].age}" alt="city name" />
+                            <img src="${data[i].img}" alt="Picture of ${data[i].name}" />
 							<h3>${data[i].name}</h3>
 							<div>${data[i].country}</div>
 					  </li>
@@ -27,6 +27,16 @@
                       </div>`
 			table.innerHTML += tile
 		}
-	}
+    }
+    
+     function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 46.619261,
+                    lng: -33.134766
+                }
+            });
+        }
 
     
