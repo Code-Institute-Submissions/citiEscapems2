158b710f171972da927404c8dee28c66632f5a2d
TheBigBushman <alanbushell91@gmail.com>
2020-06-23 15:04:21 +0000

    updated map to render markers and clusters

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ cityTile(myCity)
 
 		for (var i = 0; i < data.length; i++){
 			var tile = `<div class="row">
-            <div class="col col-sm-12 col-md-6 col-lg-3>
+                        <div class="col col-sm-12 col-md-6 col-lg-3>
                             <li class="tile">
                             <img src="${data[i].img}" alt="Picture of ${data[i].name}" />
 							<h3>${data[i].name}</h3>
@@ -30,13 +30,34 @@ cityTile(myCity)
     }
     
      function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
-        }
 
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: {lat: -28.024, lng: 140.887}
+  });
+
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+var locations = [
+  {lat: -8.409518, lng: 115.0920},
+  {lat: -33.918861, lng: 18.423300},
+  {lat: 16.5004, lng: 151.7415}
+  
+]
     
