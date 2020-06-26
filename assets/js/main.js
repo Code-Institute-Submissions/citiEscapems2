bb5bce24f8baa62830f4b7790ddc9fafef94bcb6
TheBigBushman <alanbushell91@gmail.com>
2020-06-26 10:02:51 +0000

    updated layout and formatting for widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,15 +17,17 @@ const URL = "cities.json"
 cityTile(myCity)
 
 	function cityTile(data){
-		var table = document.getElementById('myTile')
+		var table = document.getElementById('cityTiles')
 
 		for (var i = 0; i < data.length; i++){
-			var tile = `<li class="tile">
+			var tile = `<div class="col-sm-12 col-md-6 col-lg-3 mt-5">
+            <div class="container">
                             <a href="#"><img class="tile-img" src="${data[i].img}" alt="Picture of ${data[i].name}"/></a>
-							<h3 class="tile-city">${data[i].name}</h3>
-							<h5 class="tile-country">${data[i].country}</h5>
-					  </li>
+							<div class="city-centered"><h3 class="tile-city">${data[i].name}</h3></div>
+							<div class="bottom"><h5 class=tile-country>${data[i].country}</h5></div>
+					  </div>
                       </div>
+                      
                       `
 			table.innerHTML += tile
 		}
